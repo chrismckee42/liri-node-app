37116799fcc238d4534501662190b3869e308545
Christopher McKee <chris.mckee.13@gmail.com>
2019-12-03 20:37:32 -0800

    adds packages and starter text

---
+++ liri.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+
+
+
+switch (process.argv[2]) {
+    case "concert-this":
+
+        break;
+    case "concert-this":
+
+        break;
+    case "concert-this":
+
+        break;
+    case "concert-this":
+
+        break;
+
+    default:
+        break;
+}
