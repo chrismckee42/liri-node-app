9cb90cacf1e18fa8a0e96d74421a942a9748f56a
Christopher McKee <chris.mckee.13@gmail.com>
2019-12-10 20:53:39 -0800

    adds functional spotify-this code

--- liri.js
+++ liri.js
@@ -5,34 +5,45 @@ var spotify = new Spotify(keys.spotify);
 
 
 const song = function (input) {
-
     spotify
-        .search({ type: 'track', query: 'All the Small Things' })
+        .search({ type: 'track', query: process.argv[3] || "The Sign ace of base", limit: `1` })
         .then(function (response) {
-            console.log(response);
+            //console.log(JSON.stringify(response, null, 2));
+            console.log(response.tracks.items[0].album.artists[0].name);
+            console.log(response.tracks.items[0].name);
+            console.log(response.tracks.items[0].album.name);
+            console.log(response.tracks.items[0].external_urls.spotify);
         })
         .catch(function (err) {
             console.log(err);
         });
 }
 
+const concert = function (input) {
+
+}
+
+
+const movie = function (input) {
+
+}
 
 switch (process.argv[2]) {
-    case "concert-this":
-        concert(process.argv[3])
-        break;
+    // case "concert-this":
+    //     concert(process.argv[3])
+    //     break;
 
     case "spotify-this-song":
         song(process.argv[3])
         break;
 
-    case "movie-this":
-        movie(process.argv[3])
-        break;
+    // case "movie-this":
+    //     movie(process.argv[3])
+    //     break;
 
-    case "do-what-it-says":
-        randomTxt()
-        break;
+    // case "do-what-it-says":
+    //     randomTxt()
+    //     break;
 
     default:
         break;
