f61c5576cb2e9a3537000d76455c5610e10f2834
Christopher McKee <chris.mckee.13@gmail.com>
2019-12-12 08:20:35 -0800

    adds working movie code

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var Spotify = require('node-spotify-api');
+var axios = require("axios");
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
@@ -6,7 +7,7 @@ var spotify = new Spotify(keys.spotify);
 
 const song = function (input) {
     spotify
-        .search({ type: 'track', query: process.argv[3] || "The Sign ace of base", limit: `1` })
+        .search({ type: 'track', query: input || "The Sign ace of base", limit: `1` })
         .then(function (response) {
             //console.log(JSON.stringify(response, null, 2));
             console.log(response.tracks.items[0].album.artists[0].name);
@@ -26,6 +27,26 @@ const concert = function (input) {
 
 const movie = function (input) {
 
+    var queryUrl = "http://www.omdbapi.com/?t=" + input || "Mr. Nobody" + "&y=&plot=short&apikey=trilogy";
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log("Release Year: " + response.data.Year);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
 }
 
 switch (process.argv[2]) {
@@ -37,9 +58,9 @@ switch (process.argv[2]) {
         song(process.argv[3])
         break;
 
-    // case "movie-this":
-    //     movie(process.argv[3])
-    //     break;
+    case "movie-this":
+        movie(process.argv[3])
+        break;
 
     // case "do-what-it-says":
     //     randomTxt()
